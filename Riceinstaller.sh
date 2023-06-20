ebd12685216980b8ebe4e2afd9550ba06c75288a
BibooChi <137045353+BibooChi@users.noreply.github.com>
2023-06-20 16:02:47 +0530

    fixing it

--- Riceinstaller.sh
+++ Riceinstaller.sh
@@ -2,113 +2,92 @@ echo "Enjoy :grin:"
 sleep 1
 clear
 
-# Script for installing the rice
+#Start
 
-mkdir -p ~/yay
+#making necessary folders
+mkdir -p ~/.yay
+mkdir -p ~/Pictures
 
-# system update
-
+#updating the system
+echo "Updating your stuff"
 sudo pacman --noconfirm -Syu
 
-# yay installation
+
+#aur
 if ! command -v yay &> /dev/null
 then
     echo "Installing yay"
-        git clone https://aur.archlinux.org/yay.git ~/yay
-        (cd ~/yay/ && makepkg -si )
+        git clone https://aur.archlinux.org/yay.git ~/.yay/yay
+        (cd ~/.yay/yay/ && makepkg -si )
 fi
-# Dependecies moment
-
-sudo pacman --noconfirm -S alacritty dmenu dunst picom feh flameshot
 yay -S --noconfirm polybar
 
-# Rice time!!!
+#important packages
+echo "Installing necessary packages"
+sudo pacman --noconfirm -S alacritty feh dmenu dunst picom flameshot
 
-git clone https://github.com/BibooChi/I3wm-rice.git
 
-# Config file tranferring
 
-cd config
+#Installing picom config
 if [ -f ~/.config/picom.conf ]; then
     echo "picom conf backed up and new config installed"
     cp ~/.config/picom.conf ~/.config/picom.conf.bak
-    cp /picom/picom.conf ~/.config/picom.conf
+    cp config/picom.conf ~/.config/picom.conf
 else
     echo "picom conf installed"
     mkdir -p ~/.config/picom
-    cp /picom/picom.conf ~/.config/picom.conf
+    cp config/picom.conf ~/.config/picom.conf
 fi
 
+#Installing alacritty config
 if [ -f ~/.config/alacritty/alacritty.yml ]; then
     echo "alacritty config backed up and new config installed"
     cp ~/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml.bak
-    cp alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+    cp config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 else
     echo "alacritty config installed"
     mkdir -p ~/.config/alacritty
-    cp alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+    cp config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 fi
 
+#Installing dunst config
 if [ -d ~/.config/dunst ]; then
     echo "dunst config backed up and new config installed"
     mkdir ~/.config/dunst.bak && mv ~/.config/dunst/* ~/.config/dunst.bak/
-    cp -r dunst/* ~/.config/dunst
+    cp -r config/dunst/* ~/.config/dunst
 else
     echo "dunst config installed"
     mkdir ~/.config/dunst
-    cp -r dunst/* ~/.config/dunst
+    cp -r config/dunst/* ~/.config/dunst
 fi
 
+#Adding wallpapers
+if [ -d ~/Pictures/wallpapers ]; then
+    echo "adding wallpapers"
+    cp wallpapers/houses.png ~/Pictures/wallpapers/
+else
+    echo "adding wallpapers"
+    mkdir ~/Pictures/wallpapers && cp -r wallpapers/houses.png ~/Pictures/wallpapers/;
+fi
+
+#Installing i3 config
 if [ -f ~/.config/i3/config ]; then
     echo "i3 config backed up and new config installed"
     cp ~/.config/i3/config ~/.config/i3/config.bak
-    cp i3/config ~/.config/i3/config
+    cp config/i3/config ~/.config/i3/config
 else
     echo "i3 config installed"
-    cp i3/config ~/.config/i3/config
+    cp config/i3/config ~/.config/i3/config
 fi
 
-cd
 
-if [ -d ~/Pictures ]; then
-    echo "adding wallpapers"
-    cp houses.png  ~/Pictures
+#Installing polybar config
+if [ -d ~/.config/polybar ]; then
+    echo "polybar config backed up and new config installed"
+    mkdir ~/.config/polybar.bak && mv ~/.config/polybar/* ~/.config/polybar.bak/
+    cp -r config/polybar/forest ~/.config/polybar
 else
-    echo "adding wallpapers"
-    mkdir ~/Pictures && cp -r houses.png ~/Pictures;
+    echo "polybar config installed"
+    mkdir ~/.config/polybar
+    cp -r config/polybar/forest ~/.config/polybar
 fi
-
-cd config
-
-clear
-echo "LAST STEP!!!"
-echo "What's your resolution"
-echo "[1] 1366x768"
-echo "[2] 1920x1080"
-read res
-if [[ "$res" == "1" ]]; then
-    if [ -d ~/.config/polybar ]; then
-        echo "polybar config backed up and new config installed"
-        mkdir ~/.config/polybar.bak && mv ~/.config/polybar/* ~/.config/polybar.bak/
-        cp -r polybar/forest ~/.config/polybar
-    else
-        echo "polybar config installed"
-        mkdir ~/.config/polybar
-        cp -r polybar/forest ~/.config/polybar
-    fi
-elif [[ $res == "2" ]]; then
-        if [ -d ~/.config/polybar ]; then
-        echo "polybar config backed up and new config installed"
-        mkdir ~/.config/polybar.bak && mv ~/.config/polybar/* ~/.config/polybar.bak/
-        cp -r polybar/forest-1080p ~/.config/polybar/forest
-    else
-        echo "polybar config installed"
-        mkdir ~/.config/polybar
-        cp -r polybar/forest-1080p ~/.config/polybar/forest
-    fi
-else
-    echo -e "\n:/ bro enter a correct option, run the script again, thats your punishment\n"
-    exit 1
-fi
-
-echo "Thank you bbg for using the script"
